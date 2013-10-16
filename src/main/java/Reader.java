d476f9c922b4d97854266a5122b088c490b29b6a
developercyrus <developer.cyrus@gmail.com>
2013-10-17 00:10:51 +0800

    @3300

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 public class Reader {
 
 	public static void main(String[] args) throws IOException {
-		String path = Reader.class.getResource("cj5-21000").getFile();
+		String path = Reader.class.getResource("cj5-21000.txt").getFile();
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-16LE"));
 		String line;		
 		while ((line = br.readLine()) != null) {
